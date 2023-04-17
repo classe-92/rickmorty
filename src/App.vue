1b95fb7bc0d7160a622dd010a39677992d1c1d44
klelia <clelia.fradella@gmail.com>
2023-04-17 11:20:13 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <HeaderComponent />
+</template>
+
+<script>
+import axios from 'axios';
+import HeaderComponent from './components/HeaderComponent.vue';
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    getCharacters() {
+      axios.get('https://rickandmortyapi.com/api/character').then((res) => {
+        console.log(res.data.results)
+      });
+    }
+  },
+  mounted() {
+    this.getCharacters();
+  }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
